fc6d596a0e36ea25a152ef27d92ebce91eb47f07
Kongpj <pasakon_paingjai@cmu.ac.th>
2021-07-15 18:34:10 +0700

    4.6

--- src/views/Categolies.vue
+++ src/views/Categolies.vue
@@ -1,5 +1,65 @@
 <template>
   <div class="Cartegolies">
-    <h1>This is an about page</h1>
+    <Categories
+      v-for="category in categolies"
+      :key="category.id"
+      :category="category"
+    />
   </div>
 </template>
+<script>
+import Categories from '@/components/Categories.vue'
+export default {
+  components: {
+    Categories
+  },
+  data() {
+    return {
+      categolies: [
+        {
+          id: 5928101,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline friend at this event.',
+          location: 'Meow Town',
+          date: 'January 28, 2022',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee'
+        },
+        {
+          id: 4582797,
+          category: 'food',
+          title: 'Community Gardening',
+          description: 'Join us as we tend to the community edible plants.',
+          location: 'Flora City',
+          date: 'March 14, 2022',
+          time: '10:00',
+          petsAllowed: true,
+          organizer: 'Fern Pollin'
+        },
+        {
+          id: 8419988,
+          category: 'sustainability',
+          title: 'Beach Cleanup',
+          description: 'Help pick up trash along the shore.',
+          location: 'Playa Del Carmen',
+          date: 'July 22, 2022',
+          time: '11:00',
+          petsAllowed: false,
+          organizer: 'Carey Wales'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.Cartegolies {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* border: 1px solid #3c07b8; */
+}
+</style>
