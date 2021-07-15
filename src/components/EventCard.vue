74cf483c611d5a95a0cdc2d46d26bddde29b1723
Kongpj <pasakon_paingjai@cmu.ac.th>
2021-07-15 14:37:40 +0700

    3.1

---
+++ src/components/EventCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="hello"></div>
+</template>
+<script>
+export default {
+  name: 'EventCard'
+//   props: {
+//      msg: String
+//     }
+}
+</script>
+<script scoped></script>
