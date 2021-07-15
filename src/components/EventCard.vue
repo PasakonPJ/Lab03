d9160c3b37367ebb6d24fdb71e37bd55dadce6d7
Kongpj <pasakon_paingjai@cmu.ac.th>
2021-07-15 14:59:24 +0700

    3.2

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="hello"></div>
+  <div class= "event-card">
+
+  </div>
 </template>
+
 <script>
 export default {
   name: 'EventCard'
@@ -9,4 +12,21 @@ export default {
 //     }
 }
 </script>
-<script scoped></script>
+
+
+<style scoped>
+
+  .event-card 
+  {
+    padding: 20 px;
+    width: 250 px;
+    cursor: pointer;
+    border: 1px solid #39495c;
+    margin-bottom: 18 px;
+    }
+    .event-card :hover{
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0,0,0,0.2);
+    }
+
+</style>
